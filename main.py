58cddfece8a3449f1b8fab4a9e6b8075733b97ae
Teste 01 <hello02@exemplo.br>
2023-08-27 17:14:58 -0300

    pegar dados googlefinance e Bolsas MetaTrader5

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
-import MetaTrader5 as mt5
 import os
-from datetime import datetime
+import pandas as pd
+import MetaTrader5 as mt5
+
+from googleFinance import getEBIT_ROIC 
+from getAtivosDF import geraDFAtivos
+
 os.system('cls' if os.name == 'nt' else 'clear')
 
-mt5.initialize() #inicializar
+mt5.initialize(login=612347195,server='XPMT5-DEMO',password="PassTeste1()()") #inicializar
+
+dataSet = []
+
+listaAtivos = ['PETR4','ABEV3','AAPL34','M1TA34']
+
+EBIT_ROICList = getEBIT_ROIC([
+	{'ativo' : 'PETR4', 'linkAtivoGoogle' : 'PETR4:BVMF'},
+	{'ativo' : 'ABEV3', 'linkAtivoGoogle' : 'ABEV3:BVMF'},
+	{'ativo' : 'AAPL34', 'linkAtivoGoogle' : 'AAPL34:BVMF'},
+	{'ativo' : 'M1TA34', 'linkAtivoGoogle' : 'M1TA34:BVMF'}
+	])
 
 
-terminal = mt5.terminal_info()._asdict() #Apresentando opções do terminal
+for x in range(len(listaAtivos)):
+	ativoTEMP = listaAtivos[x]
+	dataFrameTMP = geraDFAtivos(ativoTEMP)
+	dataFrameTMP['EBIT'] = EBIT_ROICList[ativoTEMP]['EBIT']
+	dataFrameTMP['ROIC'] = EBIT_ROICList[ativoTEMP]['ROIC']
+	dataSet.append(dataFrameTMP)
+
+#['time', 'bid', 'ask', 'last', 'volume', 'time_msc', 'flags', 'volume_real', 'ativo', 'EBIT', 'ROIC', 'ranking_ev_ebit']
+dataSet = pd.concat(dataSet)
+print(dataSet[dataSet['ativo'] == 'PETR4'])
+
+dataSet['ranking_ev_ebit'] = dataSet.groupby('ativo')['EBIT'].rank(ascending = False)
+dataSet['ranking_roic'] = dataSet.groupby('ativo')['ROIC'].rank(ascending = False)
+dataSet['ranking_final'] = dataSet['ranking_ev_ebit'] + dataSet['ranking_roic']
+dataSet['ranking_final'] = dataSet.groupby('ativo')['ranking_final'].rank()
 
 
-#ativos = mt5.symbols_get()
-
-#print([x.name for x in ativos])
-
-EURUSD = mt5.symbol_info("EURUSD")
-
-print(EURUSD._asdict()) # Convertendo para dicionário
 
 
-mt5.shutdown()
+#IBOVESPA
+dataIbov = geraDFAtivos('IBOV')
+print(dataIbov['last'])
+
+
+
